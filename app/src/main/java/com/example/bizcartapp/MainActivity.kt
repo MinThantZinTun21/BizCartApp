d1078474a061c78e669f90ee5038d725b9335017
LENOVO <minthantzintun21@gmail.com>
2025-04-16 22:02:20 +0630

    profile view

--- app/src/main/java/com/example/bizcartapp/MainActivity.kt
+++ app/src/main/java/com/example/bizcartapp/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.example.bizcartapp
 
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bizcartapp.ui.theme.BizCartAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +49,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BizCartAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    CreateBizCard()
                 }
             }
         }
@@ -31,17 +58,127 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(300.dp)
+                .height(300.dp)
+                .padding(12.dp),
+            elevation = CardDefaults.elevatedCardElevation(4.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = CardDefaults.cardColors(Color.White)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ProfileImage()
+                Divider(
+                    thickness = 1.dp, color = Color.Gray, modifier = Modifier.padding(top = 28.dp)
+                )
+                InfoView()
+            }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BizCartAppTheme {
-        Greeting("Android")
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+
+    ) {
+
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(3.dp),
+            shape = RoundedCornerShape(8.dp),
+            border = BorderStroke(1.dp, color = Color.Gray)
+        ) {
+            Profolio(arrayListOf("Profolio", "Profolio", "Profolio"))
+
+        }
+    }
+
+}
+
+@Composable
+private fun Profolio(data: List<String>) {
+    LazyColumn() {
+        item(data) {
+            Text(text = "Profolio")
+
+        }
+
+
+    }
+
+}
+
+@Composable
+private fun InfoView() {
+    Column(
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Text(
+            text = "Min Thant zin tun ",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "I am android develper @ \n company with 10 year experience ",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Button(onClick = {}) {
+            Text(text = "Profile Button")
+        }
+
+
     }
 }
+
+@Composable
+private fun ProfileImage(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.Gray),
+        tonalElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+
+
+    ) {
+        Image(
+            painter = painterResource(R.drawable.profile),
+            contentDescription = "Profile",
+            modifier = Modifier.size(156.dp),
+            contentScale = ContentScale.FillBounds
+        )
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun BizCardDefaultPreview() {
+    CreateBizCard()
+
+}
+
